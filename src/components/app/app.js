8197acbfc01ebfb46fa0ebb4630cf225bb2991d4
«Diana» <ustiandiana@gmail.com>
2022-04-25 23:15:11 +0600

    example of css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,10 @@
+import Header from "../header-main";
+
 function App() {
   return (
+    
     <div className="App">
+      <Header />
       Welcome to our future Project
     </div>
   );
---
+++ src/components/header-main/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './header.css';
+
+const Header = () => {
+    return (
+      <div>
+          chto-to
+      </div>
+    );
+  };
+
+export default Header;
