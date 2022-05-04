8fa44a41c8c7be8d3764f2bcaee2d7e4eb102e60
«Diana» <ustiandiana@gmail.com>
2022-05-04 17:57:54 +0600

    add slider-part

--- src/components/main_page/slider/slider.js
+++ src/components/main_page/slider/slider.js
@@ -51,37 +51,52 @@ const Slider = () => {
             
         </div>
 
-            <div className="head">
-                <h1> TOP SELLERS </h1>
-            </div>
-            <div className="slider-container">
-                <div className="imagesList">
-                    <div className="prev">
-                        <img src={require('./img-slider/Vector.jpg')} alt=""/>
-                    </div>
-                    <div className="img_text">
-                        <img src={require('./img-slider/adidas.jpg')}alt="" />
-                        <h1>Adidas</h1>
-                        <p>San Francisco Baseball</p>
-                        <span className="yellow">3800c</span>
-                    </div>
-                    <div className="img_text">
-                        <img src={require('./img-slider/new_era.jpg')} alt=""/>
-                        <h1>New Era</h1>
-                        <p>New York Yankies</p>
-                        <span className="yellow">3800c</span>
-                    </div>
-                    <div className="img_text">
-                        <img src={require('./img-slider/nike.jpg')} alt=""/>
-                        <h1>Nike</h1>
-                        <p>French Fries Series</p>
-                        <span className="yellow">4500c</span>
-                    </div>
-                    <div className="next">
-                        <img src={require('./img-slider/Vector_2.jpg')} alt=""/>
-                    </div>
+        <div className='small-caps-container'>
+            <div className='small-c-custom'>
+                <div className='small-c-desc'>
+                    <h3>Кастомные</h3>
+                    <p>Создай свой стиль</p>
                 </div>
             </div>
+            <div className='small-c-exclusive'>
+                <div className='small-c-desc'>
+                    <h3>Эксклюзив</h3>
+                    <p>Найди уникальные кепки</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="head">
+            <h1> TOP SELLERS </h1>
+        </div>
+        <div className="slider-container">
+            <div className="imagesList">
+                <div className="prev">
+                    <img src={require('./img-slider/Vector.jpg')} alt=""/>
+                </div>
+                <div className="img_text">
+                    <img src={require('./img-slider/adidas.jpg')}alt="" />
+                    <h1>Adidas</h1>
+                    <p>San Francisco Baseball</p>
+                    <span className="yellow">3800c</span>
+                </div>
+                <div className="img_text">
+                    <img src={require('./img-slider/new_era.jpg')} alt=""/>
+                    <h1>New Era</h1>
+                    <p>New York Yankies</p>
+                    <span className="yellow">3800c</span>
+                </div>
+                <div className="img_text">
+                    <img src={require('./img-slider/nike.jpg')} alt=""/>
+                    <h1>Nike</h1>
+                    <p>French Fries Series</p>
+                    <span className="yellow">4500c</span>
+                </div>
+                <div className="next">
+                    <img src={require('./img-slider/Vector_2.jpg')} alt=""/>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
